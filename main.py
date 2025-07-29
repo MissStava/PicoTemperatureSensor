06e01c535abc2cd2e99fb748956e643456c81650
Isla Hilton <null.method@gmail.com>
2025-07-29 23:32:00 +0100

    Clean up imports

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-import time
+from time import sleep
 
 from machine import Pin
-import onewire
-import ds18x20
+from onewire import OneWire
+from ds18x20 import DS18X20
+
 
 pin = Pin(27, Pin.IN)
 print(pin.value())
 
-one_wire = onewire.OneWire(Pin(pin))
-sensor = ds18x20.DS18X20(one_wire)
+one_wire = OneWire(Pin(pin))
+sensor = DS18X20(one_wire)
 devices = sensor.scan()
 
-print("Found devices:", devices)
+
+print("Found devices: ", devices)
 for device in devices:
     while True:
         sensor.convert_temp()
-        time.sleep(1)
+        sleep(1)
         temp = sensor.read_temp(device)
         print("Temperature:", temp)
