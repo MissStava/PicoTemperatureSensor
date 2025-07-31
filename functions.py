543912ebdcb9d57e26933ea2ca3446fb9e55253f
Isla Hilton <null.method@gmail.com>
2025-07-31 22:36:49 +0100

    Aff error handling and structure to wifi connection function

--- functions.py
+++ functions.py
@@ -2,32 +2,64 @@ from time import sleep
 
 import secrets
 
-from network import WLAN
+import network
 from machine import Pin
 from onewire import OneWire
 from ds18x20 import DS18X20
 
+WIFI_CONNECTION_ATTEMPTS = 2
+WIFI_CONNECTION_WAIT = 10
 
-def connect_to_wifi():
+
+def connect_to_wifi(
+    attempts: int = WIFI_CONNECTION_ATTEMPTS, wait: int = WIFI_CONNECTION_WAIT
+):
     """
     Connects the device to a WiFi network using credentials from the secrets module.
 
-    This function activates the WLAN interface in station mode, attempts to connect to
-    the WiFi network using the provided SSID and password, and waits until the connection
-    is established.
+    Args:
+        attempts (int): Number of connection attempts.
+        wait (int): Seconds to wait for each attempt.
     """
 
     print("Connecting to WiFi")
 
-    wlan = WLAN(WLAN.IF_STA)
+    wlan = network.WLAN(network.WLAN.IF_STA)
     wlan.active(True)
-    if not wlan.isconnected():
-        wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-        while not wlan.isconnected():
-            print("Connecting")
-            sleep(1)
 
-    print("Network config:", wlan.ipconfig("addr4"))
+    if wlan.isconnected():
+        print("Already connected")
+        return wlan
+
+    for attempt in range(1, attempts + 1):
+        print(f"WiFi attempt {attempt} of {attempts}")
+        wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+
+        for _ in range(wait):
+            if wlan.status() == network.STAT_CONNECTING:
+                print("Connecting...")
+                sleep(1)
+            else:
+                break
+
+        status = wlan.status()
+        if status == network.STAT_GOT_IP:
+            print("Network config:", wlan.ifconfig())
+            return wlan
+
+        if status == network.STAT_WRONG_PASSWORD:
+            print("Connection failed: Wrong WiFi password.")
+            raise RuntimeError("Wrong WiFi password")
+
+        if status == network.STAT_NO_AP_FOUND:
+            print("Connection failed: Access point not found.")
+            raise RuntimeError("Access point not found")
+
+        print("Connection failed.")
+        sleep(1)
+
+    print("Failed to connect to WiFi.")
+    raise RuntimeError("Failed to connect to WiFi")
 
 
 def initialise_sensor(pin):
