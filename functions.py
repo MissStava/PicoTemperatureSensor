1b87beced779db83f6afe8cf7de549849798e0e6
Isla Hilton <null.method@gmail.com>
2025-08-07 21:02:38 +0100

    Refacot sensor initialise function

--- functions.py
+++ functions.py
@@ -62,12 +62,12 @@ def connect_to_wifi(
     raise RuntimeError("Failed to connect to WiFi")
 
 
-def initialise_sensor(pin):
+def initialise_sensor(pin_number):
     """
     Initialises a DS18X20 temperature sensor on the specified GPIO pin.
 
     Args:
-        pin (int): The GPIO pin number to which the sensor is connected.
+        pin_number (int): The GPIO pin number to which the sensor is connected.
 
     Returns:
         DS18X20: An instance of the DS18X20 sensor initialised on the specified pin.
@@ -75,6 +75,9 @@ def initialise_sensor(pin):
 
     print("Initialising sensor")
 
-    one_wire = OneWire(Pin(pin))
+    if pin_number is None:
+        raise ValueError("Pin number must be specified to initialise the sensor")
 
+    pin = Pin(pin_number)
+    one_wire = OneWire(pin)
     return DS18X20(one_wire)
